11494b61229cf1d4dc229f44f6ae73ea91b13d60
Darek <dardman88@gmail.com>
2020-04-14 12:57:25 +0200

    Add favicon

---
+++ build/precache-manifest.3e17d2a8ebc0f51ab3d2d67fa5a254ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d4b2250a3fe25e917558457b85589c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e75a2e637db2a5ec09",
+    "url": "/static/css/main.29f6e666.chunk.css"
+  },
+  {
+    "revision": "928935af26c852c175e6",
+    "url": "/static/js/2.742c8bd0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.742c8bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e75a2e637db2a5ec09",
+    "url": "/static/js/main.5a5ab130.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
